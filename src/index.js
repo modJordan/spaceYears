712bc06a58268b550d648c6e60f5405ec6215631
modjordan <mod.jordan@icloud.com>
2023-07-27 13:20:29 -0700

    Bad fail for User class

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-//import, file name and path for other js files
-
-// js goes here
+import "./js/logic.js";
